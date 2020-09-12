fc5d094f29a19894913ab18984f5315f362fef85
Mark1626 <nimalan.m@protonmail.com>
2020-09-12 19:34:00 +0530

    Add vanilla pwa

---
+++ vanilla-pwa/sw.js
@@ -0,0 +1,15 @@
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open("pwa-assets").then((cache) => {
+      return cache.addAll(["index.html", "index.css"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
